33d06af19c8a3d97b6a67f95069f041430a28f94
Iammutevu <ianmutevu96@gmail.com>
2020-06-19 13:25:18 +0300

    Bulk sending added

--- airtime-sender/sendAirtime.php
+++ airtime-sender/sendAirtime.php
@@ -1,27 +1,53 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
 <?php
-require 'vendor/autoload.php';
-use AfricasTalking\SDK\AfricasTalking;
 
-$username = "sandbox";
-$apiKey = "2607d14a0f69a5bea0ee61b213dcb2bdb5675cbc0e0375625d77eb7ccd1cee86";
+require "helpers/AirtimeSender.php";
 
-$AT = new AfricasTalking($username, $apiKey);
+//create object from sender class
+$airtimeObj = new AirtimeSender();
 
-$airtime = $AT->airtime();
+//get values from POST request
+$total = $_POST['total'];
 
+$phoneArray = array();
+$amountArray = array();
+$networkArray = array();
 
-
-$recipients = [[
-"phoneNumber"  => $_POST["phone"],
-"currencyCode"       => "KES",
-"amount"       =>$_POST["amount"]
-]];
-try {
-    $results = $airtime->send([
-        "recipients" => $recipients
-    ]);
-    print_r($results);
-} catch(Exception $e) {
-    echo "Error: ".$e->getMessage();
+//feed values to arrays
+for($i = 0; $i <= $total; $i++){
+	array_push($phoneArray, $_POST["phone".$i]);
+	array_push($amountArray, $_POST["amount".$i]);
+	array_push($networkArray, $_POST["network".$i]);
 }
+
+
+//$airtimeObj->sendAirtime($phone, $amount);
+
+//loop through recipients sending airtime to each
+
+for($i = 0; $i <= $total; $i++){
+	$response = $airtimeObj->sendAirtime($phoneArray[$i], $amountArray[$i]);
+	echo $response["status"] ."<br>";
+}
+
+
+?>
+<br>
+<div class="row">
+	<a class="btn btn-info btn-block" href="index.php">Send again</a>	
+</div>
+
+	</body>
+</html>
+
 
--- airtime-sender/vendor/composer/autoload_psr4.php
+++ airtime-sender/vendor/composer/autoload_psr4.php
@@ -6,13 +6,19 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'ZipStream\\' => array($vendorDir . '/maennchen/zipstream-php/src'),
     'Symfony\\Polyfill\\Php72\\' => array($vendorDir . '/symfony/polyfill-php72'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Polyfill\\Intl\\Idn\\' => array($vendorDir . '/symfony/polyfill-intl-idn'),
+    'Psr\\SimpleCache\\' => array($vendorDir . '/psr/simple-cache/src'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
+    'PhpOffice\\PhpSpreadsheet\\' => array($vendorDir . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet'),
+    'MyCLabs\\Enum\\' => array($vendorDir . '/myclabs/php-enum/src'),
+    'Matrix\\' => array($vendorDir . '/markbaker/matrix/classes/src'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
+    'Complex\\' => array($vendorDir . '/markbaker/complex/classes/src'),
     'AfricasTalking\\SDK\\Tests\\' => array($vendorDir . '/africastalking/africastalking/tests'),
     'AfricasTalking\\SDK\\' => array($vendorDir . '/africastalking/africastalking/src'),
 );
